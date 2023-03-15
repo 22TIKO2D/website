d54ef3e7454262c0bc0a0fc950120018225c582e
Jarkko Kuukkanen <jarkko.kuukkanen@tuni.fi>
2023-03-15 20:58:37 +0200

    chore: preload fonts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,10 @@
   import "@fontsource/poppins/latin-700.css";
   import "@fontsource/poppins/latin-900.css";
 
+  import font400 from "@fontsource/poppins/files/poppins-latin-400-normal.woff2";
+  import font700 from "@fontsource/poppins/files/poppins-latin-700-normal.woff2";
+  import font900 from "@fontsource/poppins/files/poppins-latin-900-normal.woff2";
+
   // And Windi CSS.
   import "virtual:windi.css";
   import "$lib/style.css";
@@ -13,6 +17,13 @@
   import Footer from "$lib/Footer.svelte";
   import BackTop from "$lib/BackTop.svelte";
 </script>
+
+<!-- Preload fonts so they don't switch after page load. -->
+<svelte:head>
+  <link rel="preload" as="font" href={font400} type="font/woff2" crossorigin />
+  <link rel="preload" as="font" href={font700} type="font/woff2" crossorigin />
+  <link rel="preload" as="font" href={font900} type="font/woff2" crossorigin />
+</svelte:head>
 
 <!-- When the page is smaller than screen height, grow to the screen height. -->
 <div class="flex flex-col min-h-screen">
